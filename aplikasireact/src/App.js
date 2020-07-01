b1b217674f4a57d685eee65e9f0b854ffb5c25e8
Bilqist <bilqistaisaah@gmail.com>
2020-07-01 11:11:56 +0700

    Kuliah Web #24 Passing Data Antar Komponent

--- aplikasireact/src/App.js
+++ aplikasireact/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Home from './Component/fungsional/Home';
 import Beranda from './Component/class/Beranda';
-
+import Parent from './Component/class/Parent';
+//import logo from './logo.svg';
+//import Home from './Component/Fungsional/Home';
+//import Beranda from './Component/Class/Beranda';
+//import './App.css';
 function App() {
   return (
     <div className="App">
-    <Home/>
-    <Beranda/>
+      <Home/>
+      <Parent/>
+      <Beranda/>
     </div>
   );
-} 
-
+}
 export default App;
