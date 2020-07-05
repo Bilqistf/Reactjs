9a20d4c2441cf9cab42a4a17613faee5db3a8f64
Bilqist <bilqistaisaah@gmail.com>
2020-07-05 18:09:04 +0700

    Kuliah Web #29 dan #30

--- aplikasireact/src/App.js
+++ aplikasireact/src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import NavbarComp from './Component/fungsional/NavbarComp';
 import HomePage from './Component/fungsional/HomePage';
 import About from './Component/fungsional/AboutComp';
-import DetailComp from './Component/fungsional/DetailComp';
+import ListComp from './Component/class/ListComp';
 
 const App = () => {
   return (<BrowserRouter >
@@ -12,7 +12,7 @@ const App = () => {
     <Switch >
       <Route exact path="/"component={HomePage}/> 
       <Route exact path="/about"component={About}/> 
-      <Route exact path="/detail/:id"component={DetailComp}/> 
+      <Route exact path="/mahasiswa"component={ListComp}/> 
       </Switch>
       </BrowserRouter>
   );
--- aplikasireact/src/Component/fungsional/NavbarComp.jsx
+++ aplikasireact/src/Component/fungsional/NavbarComp.jsx
@@ -25,6 +25,9 @@ const NavbarComp = (props) => {
                         <NavItem>
                             <NavLink href="/about" >About</NavLink>
                         </NavItem>
+                        <NavItem>
+                            <NavLink href="/mahasiswa" >Mahasiswa </NavLink>
+                        </NavItem>
                     </Nav>
                     <NavbarText>Simple Text</NavbarText>
                 </Collapse>
