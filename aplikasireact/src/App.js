1e6ffd4daaa6accba52ba62212055337daffe449
Bilqist <bilqistaisaah@gmail.com>
2020-07-05 12:30:22 +0700

    Kuliah Web #27 Menggunakan React Router

--- aplikasireact/src/App.js
+++ aplikasireact/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import BootstrapComp from './Component/class/BoostrapComp';
 
-//import logo from './logo.svg';
-//import Home from './Component/Fungsional/Home';
-//import Beranda from './Component/Class/Beranda';
-//import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <BootstrapComp/>
-    </div>
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import NavbarComp from './Component/fungsional/NavbarComp';
+import HomePage from './Component/fungsional/HomePage';
+import About from './Component/fungsional/AboutComp';
+
+const App = () => {
+  return (<BrowserRouter >
+    <NavbarComp />
+    <Switch >
+      <Route exact path="/"component={HomePage}/> 
+      <Route exact path="/about"component={About}/> 
+      </Switch>
+      </BrowserRouter>
   );
 }
-export default App;
+export default App; 
